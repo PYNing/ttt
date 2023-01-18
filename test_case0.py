b8afb43f78b72e1e3ae324d65feee2057d2fca93
PYNing <540439329@qq.com>
2023-01-18 16:50:47 +0800

    Create test_case0.py

---
+++ test_case0.py
@@ -0,0 +1,35 @@
+# import sys
+# sys.path.insert(0, "/data/ningpeiyang/oneflow_before_pull/python")
+
+import oneflow as flow
+import oneflow.nn as nn
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear = nn.Linear(128, 128)
+
+    def forward(self, x):
+        x1 = self.linear(x)
+        x = nn.functional.depend(x, x1)
+        x2 = x * 2
+        return x1 + x2
+
+model = Model().cuda()
+
+class Graph(nn.Graph):
+    def __init__(self) -> None:
+        super().__init__()
+        self.model = model
+
+    def build(self, x):
+        return self.model(x)
+
+graph = Graph() 
+# graph.debug(3)
+x = flow.randn([1, 128], dtype=flow.float32).cuda()
+
+y = graph(x)
+y = graph(x)
+
